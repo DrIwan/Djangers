315ed34fe81c59106d37ed33d7edec5f06f8d3ab
Иван <124264020+DrIwan@users.noreply.github.com>
2023-05-12 15:49:32 +0500

    Delete Djangers directory

--- Djangers/project/apps/tests/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class TestsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tests'
-    verbose_name = "Тест+"
-    verbose_name_plural = "Тест+"
