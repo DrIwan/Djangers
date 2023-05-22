57db80bacbbb0b877a8560ecbfe297fa82b508a4
Иван <124264020+DrIwan@users.noreply.github.com>
2023-05-22 11:52:15 +0500

    Настройка и тестирование Django как API (#12)
    Fixes #10

--- backend/views.py
+++ backend/views.py
@@ -1,4 +1,22 @@
-from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import UserSerializer, GroupSerializer
 
-def home(request):
-    return redirect('tests:home')
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    Конечная точка API, которая позволяет пользователям просматривать или редактировать.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    Конечная точка API, которая позволяет просматривать или редактировать группы.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
