3a0723b6302c754b14527661bf1dda4ecab549ae
1KNP1 <kri_nadya@mail.ru>
2023-05-25 23:39:23 +0500

    фыыфвф

--- frontend/src/components/ProfilePage.vue
+++ frontend/src/components/ProfilePage.vue
@@ -1,5 +1,47 @@
 <template>
     <div class="Profile">
-      Profile
+        <section class="cont">
+        <div class="form-reg-log">
+            <form class="login-box">
+                <h2>Вход</h2>
+
+                <div class="text-box">
+                    <input type="text" name="" >
+                    <label>Логин</label>
+                </div>
+                <div class="text-box">
+                    <input type="text" name="">
+                    <label>Пароль</label>
+                </div>
+                <button>Войти</button>
+                <div class="open-reg">Перейти к регистрации</div>
+            </form>
+            <div class="line">
+
+            </div>
+            <form class="reg-box">
+                <h2>Регистрация</h2>
+                <div class="text-box">
+                    <input type="text" name="">
+                    <label>ФИО</label>
+                </div>
+
+                <div class="text-box">
+                    <input type="text" name="">
+                    <label>Логин</label>
+                </div>
+
+                <div class="text-box">
+                    <input type="text" name="">
+                    <label>Пароль</label>
+                </div>
+                <div class="text-box">
+                    <input type="number" name="" min="14">
+                    <label>Возраст</label>
+                </div>
+                <button>Отправить</button>
+            </form>
+        </div>
+    </section>
     </div>
   </template>
