7d3905b287bc2b0c9f3a486790df87bf99731251
Gaman <gamanov.a.a@mail.ru>
2023-05-31 14:20:48 +0500

    Немного компонентов

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,8 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from "@/router/index"
 import './index.css'
+import components from '@/components/UI'
 
 const app = createApp(App)
+
+components.forEach(component=>{
+    app.component(component.name, component)
+})
+
 app
     .use(router)
     .mount('#app')
